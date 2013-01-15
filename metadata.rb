74737313d2d88d17e7cf1f2128f75dfdd31e3ac1
Iñigo gonzalez (exocert) <igponce@gmail.com>
2013-01-15 16:55:46 +0100

    Added debian platform support (ignored today by Chef; but who knows)

--- metadata.rb
+++ metadata.rb
@@ -14,5 +14,15 @@ recipe "hadoop::hive", "Installs hadoop's hive package"
 recipe "hadoop::pig", "Installs hadoop's pig package"
 
 %w{ debian ubuntu }.each do |os|
-  supports os
+  
+  # I think this is the beginning of a beautiful bug report:
+
+   # http://wiki.opscode.com/display/chef/Metadata
+  # Currently, the platform matching and version checking logic is not implemented - Chef will happily run any cookbook on any platform, regardless of the data provided here.
+
+  ml = Mixlib::ShellOut.new("dpkg --print-architecture")
+  ml.run_command
+  debian_arch = ml.stdout unless ml.error!
+  supports os unless debian_arch != 'amd64'
+
 end
